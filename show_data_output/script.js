8670eb35b6311e2689581ae2ed4390f65b6742c9
vedantmartin <vedantmartin@gmail.com>
2023-06-08 17:22:32 +0100

    filtered some other data and added pdfs for output

--- show_data_output/script.js
+++ show_data_output/script.js
@@ -1,48 +1,75 @@
-// Set the dimensions of the SVG container
-var width = 800;
-var height = 400;
-var margin = { top: 20, right: 20, bottom: 30, left: 50 };
-
-// Create the SVG container
-var svg = d3.select("#graph-container")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height);
-
 // Load the CSV file
-d3.csv("../sensor_data.csv")
-  .then(function(data) {
-    // Parse the sensorData values as floats
+d3.csv("../sensor_data.csv").then(function(data) {
+    // Parse the sensorData values as floats and time values as dates
+    var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S.%L");
     data.forEach(function(d) {
       d.sensorData = parseFloat(d.sensorData);
+      d.time = parseTime(d.time);
     });
-
-    // Set the scales for x and y axes
-    var xScale = d3.scaleBand()
-                   .domain(data.map(function(d) { return d.time; })) // Use time as the x-axis label
-                   .range([margin.left, width - margin.right])
-                   .padding(0.1);
-
-    var yScale = d3.scaleLinear()
-                   .domain([0, d3.max(data, function(d) { return d.sensorData; })])
-                   .range([height - margin.bottom, margin.top]);
-
-    // Create the bars
-    svg.selectAll("rect")
-       .data(data)
-       .enter()
-       .append("rect")
-       .attr("x", function(d) { return xScale(d.time); })
-       .attr("y", function(d) { return yScale(d.sensorData); })
-       .attr("width", xScale.bandwidth())
-       .attr("height", function(d) { return height - margin.bottom - yScale(d.sensorData); })
-       .attr("fill", "steelblue");
-
-    // Create y-axis
-    svg.append("g")
-       .attr("transform", "translate(" + margin.left + ",0)")
-       .call(d3.axisLeft(yScale));
-  })
-  .catch(function(error) {
-    console.log("Error loading CSV file:", error);
+  
+    // Set the dimensions of the chart
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    var width = 800 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
+  
+    // Create the SVG container
+    var svg = d3
+      .select("#chart-container")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  
+    // Set the x and y scales
+    var xScale = d3
+      .scaleTime()
+      .domain(d3.extent(data, function(d) { return d.time; }))
+      .range([0, width]);
+  
+    var yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, function(d) { return d.sensorData; })])
+      .range([height, 0]);
+  
+    // Create the line generator
+    var line = d3
+      .line()
+      .x(function(d) { return xScale(d.time); })
+      .y(function(d) { return yScale(d.sensorData); });
+  
+    // Append the line chart
+    svg
+      .append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
+  
+    // Define the time ranges to highlight
+    // var timeRanges = [
+    //   { start: parseTime("2023-06-07 22:00:00"), end: parseTime("2023-06-07 23:00:00") },
+    //   { start: parseTime("2023-06-07 18:00:00"), end: parseTime("2023-06-07 19:00:00") }
+    // ];
+  
+    // // Append rectangles to highlight time ranges
+    // svg
+    //   .selectAll("rect")
+    //   .data(timeRanges)
+    //   .enter()
+    //   .append("rect")
+    //   .attr("x", function(d) { return xScale(d.start); })
+    //   .attr("y", 0)
+    //   .attr("width", function(d) { return xScale(d.end) - xScale(d.start); })
+    //   .attr("height", height)
+    //   .attr("class", "highlight");
+  
+    // Add x-axis
+    svg
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(xScale));
+  
+    // Add y-axis
+    svg.append("g").call(d3.axisLeft(yScale));
   });
+  
